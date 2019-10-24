14c3eb2347ee5a0a18e365ab494f768a4185a0d6
Alejo <alejandrofranco@pursuit.org>
2019-10-24 01:43:07 -0400

    initial migration creating base tables v1

---
+++ db/migrations/sqls/20191024042618-initial-up.sql
@@ -0,0 +1,24 @@
+CREATE TABLE todos (
+  id SERIAL PRIMARY KEY,
+  text VARCHAR NOT NULL,
+  completed BOOLEAN DEFAULT FALSE,
+  value INT NOT NULL
+);
+
+CREATE TABLE journal_entries (
+  id SERIAL PRIMARY KEY,
+  text VARCHAR NOT NULL,
+  ts TIMESTAMPTZ DEFAULT NOW()
+);
+
+CREATE TABLE tags (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(25)
+);
+
+CREATE TABLE je_tags (
+  id SERIAL PRIMARY KEY,
+  je_id INT REFERENCES journal_entries (id),
+  tag_id INT REFERENCES tags (id)
+);
+
