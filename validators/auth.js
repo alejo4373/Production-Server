9abe7b3d5f68d32a5a39613ca0c2b850e5550022
Alejo <alejandrofranco@pursuit.org>
2020-02-28 00:33:16 -0500

    trim password, email and username to prevent them being invisible/empty

--- validators/auth.js
+++ validators/auth.js
@@ -2,14 +2,14 @@ const { body } = require('express-validator');
 const withValidationErrorHandler = require('./withValidationErrorHandler');
 
 const loginValidators = withValidationErrorHandler([
-  body('username', 'username must not be empty').notEmpty(),
-  body('password', 'password must not be empty').notEmpty()
+  body('username', 'username must not be empty').trim().notEmpty(),
+  body('password', 'password must not be empty').trim().notEmpty()
 ])
 
 const signupValidators = withValidationErrorHandler([
-  body('username', 'username must not be empty').notEmpty(),
-  body('password', 'password must not be empty').notEmpty(),
-  body('email', 'email must be valid').isEmail()
+  body('username', 'username must not be empty').trim().notEmpty(),
+  body('password', 'password must not be empty').trim().notEmpty(),
+  body('email', 'email must be valid').trim().isEmail()
 ])
 
 module.exports = {
