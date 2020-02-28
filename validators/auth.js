15684422669992b2d4b10daafcb53e553df2a7d1
Alejo <alejandrofranco@pursuit.org>
2020-02-27 23:41:44 -0500

    send clearer shorter message for username & password login validation

--- validators/auth.js
+++ validators/auth.js
@@ -2,8 +2,8 @@ const { body } = require('express-validator');
 const withValidationErrorHandler = require('./withValidationErrorHandler');
 
 const loginValidators = withValidationErrorHandler([
-  body('username', 'Username must be present and not be empty').notEmpty(),
-  body('password', 'Password must be present and not be empty').notEmpty()
+  body('username', 'username must not be empty').notEmpty(),
+  body('password', 'password must not be empty').notEmpty()
 ])
 
 const signupValidators = withValidationErrorHandler([
