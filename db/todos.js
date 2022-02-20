1254f2ea949767fe4adda0081edfdb817b267060
Alejandro Franco <alejandro@flowcode.com>
2022-02-20 13:40:46 -0500

    Allow updating the list_id of a todo

--- db/todos.js
+++ db/todos.js
@@ -1,28 +1,31 @@
 const { db, helpers, pgpAs, recordNotFound, invalidInteger } = require("./pgp");
 const Tags = require("./tags");
 
-const optionalCol = col => ({
+const optionalCol = (col) => ({
   name: col,
-  skip: (col) => col.value === null || col.value === undefined || !col.exists
-})
+  skip: (col) => col.value === null || col.value === undefined || !col.exists,
+});
 
 const getAllTodos = async (queryParams) => {
-  const client_tz = queryParams.client_tz || 'UTC'
-  delete queryParams.client_tz // To avoid being used in the whereConditions
+  const client_tz = queryParams.client_tz || "UTC";
+  delete queryParams.client_tz; // To avoid being used in the whereConditions
 
-  const params = Object.keys(queryParams)
-  const timeParams = new Set(['completed_at', 'updated_at', 'due_at'])
+  const params = Object.keys(queryParams);
+  const timeParams = new Set(["completed_at", "updated_at", "due_at"]);
 
-  const whereConditions = params.map(param => {
-    let columnName = pgpAs.name(param)
-    if (timeParams.has(param)) {
-      return `(todos.${columnName} AT TIME ZONE $/client_tz/)::Date = $/${param}/` // Type cast to SQL Date AT TIME ZONE to discard time
-    } if (param === 'text') {
-      return `todos.text_searchable @@ plainto_tsquery($/${param}/)`
-    } else {
-      return `todos.${columnName} = $/${param}/`
-    }
-  }).join(' AND ')
+  const whereConditions = params
+    .map((param) => {
+      let columnName = pgpAs.name(param);
+      if (timeParams.has(param)) {
+        return `(todos.${columnName} AT TIME ZONE $/client_tz/)::Date = $/${param}/`; // Type cast to SQL Date AT TIME ZONE to discard time
+      }
+      if (param === "text") {
+        return `todos.text_searchable @@ plainto_tsquery($/${param}/)`;
+      } else {
+        return `todos.${columnName} = $/${param}/`;
+      }
+    })
+    .join(" AND ");
 
   const SQL = `
     SELECT
@@ -34,14 +37,14 @@ const getAllTodos = async (queryParams) => {
     WHERE ${whereConditions}
     GROUP BY todos.id
     ORDER BY created_at DESC
-  `
+  `;
 
   let todos = await db.any(SQL, {
     ...queryParams,
-    client_tz
+    client_tz,
   });
-  return todos
-}
+  return todos;
+};
 
 const getTodoWithTags = async (id, owner_id) => {
   let todo;
@@ -54,7 +57,7 @@ const getTodoWithTags = async (id, owner_id) => {
       LEFT JOIN tags ON todos_tags.tag_id = tags.id 
     WHERE todos.id = $/id/ AND todos.owner_id = $/owner_id/
     GROUP BY todos.id
-  `
+  `;
 
   try {
     todo = await db.one(SQL, { id, owner_id });
@@ -62,17 +65,17 @@ const getTodoWithTags = async (id, owner_id) => {
     return todo;
   } catch (err) {
     if (recordNotFound(err) || invalidInteger(err)) {
-      todo = null
+      todo = null;
       return todo;
     }
-    throw (err)
+    throw err;
   }
-}
+};
 
 const getTodosByTags = async (tags) => {
   const variables = tags.map((_, i) => {
-    return `$${i + 1}::VARCHAR`
-  })
+    return `$${i + 1}::VARCHAR`;
+  });
 
   const SQL = `
     SELECT
@@ -82,138 +85,146 @@ const getTodosByTags = async (tags) => {
           JOIN todos_tags ON todos.id = todos_tags.todo_id
           JOIN tags ON todos_tags.tag_id = tags.id
     GROUP BY todos.id
-    HAVING ARRAY[${variables.join(', ')}] <@ ARRAY_AGG(tags.name)
-  `
+    HAVING ARRAY[${variables.join(", ")}] <@ ARRAY_AGG(tags.name)
+  `;
 
   try {
-    const todos = await db.any(SQL, tags)
+    const todos = await db.any(SQL, tags);
     return todos;
   } catch (err) {
-    throw (err)
+    throw err;
   }
-}
+};
 
 const createTodo = async (todo) => {
   try {
     const SQL = `
-      INSERT INTO todos(owner_id, text, value ${todo.list_id ? ', list_id' : ''})
-        VALUES($/owner_id/, $/text/, $/value/ ${todo.list_id ? ', $/list_id/' : ''})
+      INSERT INTO todos(owner_id, text, value ${
+        todo.list_id ? ", list_id" : ""
+      })
+        VALUES($/owner_id/, $/text/, $/value/ ${
+          todo.list_id ? ", $/list_id/" : ""
+        })
       RETURNING *
-    `
-    const newTodo = await db.one(SQL, todo)
+    `;
+    const newTodo = await db.one(SQL, todo);
 
     if (!todo.tags || !todo.tags.length) {
-      todo.tags = ['untagged']
+      todo.tags = ["untagged"];
     }
 
-    const existingTags = await Tags.getTagsByName(todo.tags)
+    const existingTags = await Tags.getTagsByName(todo.tags);
 
-    // A better approach for this might be to have tags be unique 
+    // A better approach for this might be to have tags be unique
     // by their name and insert the Tags and attempt to insert all Tags
     // and let duplicates fail or take note to create the association only
     // This is not scallable if we have a ton of users creating their own unique tags.
     // All tags need to be loaded into memory.
-    const newTags = todo.tags.filter(tagName => {
-      const found = existingTags.find(t => t.name === tagName)
-      return !found
-    })
+    const newTags = todo.tags.filter((tagName) => {
+      const found = existingTags.find((t) => t.name === tagName);
+      return !found;
+    });
 
     let newInsertedTags = [];
     if (newTags.length) {
-      newInsertedTags = await Tags.createMultiple(newTags, newTodo.owner_id)
+      newInsertedTags = await Tags.createMultiple(newTags, newTodo.owner_id);
     }
 
-    let allTags = [...existingTags, ...newInsertedTags]
-    await Tags.associateWithTodo(allTags, newTodo.id)
+    let allTags = [...existingTags, ...newInsertedTags];
+    await Tags.associateWithTodo(allTags, newTodo.id);
 
     return {
       ...newTodo,
-      tags: allTags.map(t => t.name)
-    }
+      tags: allTags.map((t) => t.name),
+    };
   } catch (err) {
-    throw err
+    throw err;
   }
-}
+};
 
 const removeTodo = async (id, owner_id) => {
   let todo;
   try {
-    todo = await db.one(`DELETE FROM todos WHERE id = $/id/ AND owner_id = $/owner_id/ 
-      RETURNING *`, { id, owner_id });
+    todo = await db.one(
+      `DELETE FROM todos WHERE id = $/id/ AND owner_id = $/owner_id/ 
+      RETURNING *`,
+      { id, owner_id }
+    );
     return todo;
   } catch (err) {
     if (recordNotFound(err)) {
-      todo = false
+      todo = false;
       return todo;
     }
-    throw (err)
+    throw err;
   }
-}
+};
 
 const updateTodo = async (id, owner_id, todoEdits) => {
   const columnSet = new helpers.ColumnSet([
     optionalCol("text"),
     optionalCol("value"),
-    optionalCol('completed'),
-    optionalCol('completed_at'),
-    optionalCol('due_at')
-  ])
+    optionalCol("completed"),
+    optionalCol("completed_at"),
+    optionalCol("due_at"),
+    optionalCol("list_id"),
+  ]);
 
   const updateQuery = `
-    ${helpers.update(todoEdits, columnSet, 'todos')}
+    ${helpers.update(todoEdits, columnSet, "todos")}
     WHERE todos.id = $/id/ AND owner_id = $/owner_id/
     RETURNING *
   `;
 
   let todo;
   try {
-    todo = await db.one(updateQuery, { id, owner_id })
-    return todo
+    todo = await db.one(updateQuery, { id, owner_id });
+    return todo;
   } catch (err) {
     if (recordNotFound(err) || invalidInteger(err)) {
-      todo = null
+      todo = null;
       return todo;
     }
-    throw (err)
+    throw err;
   }
-}
+};
 
 const toggleCompleted = async (id, owner_id) => {
   try {
     const todo = await getTodoWithTags(id, owner_id);
 
-    if (!todo) return null
+    if (!todo) return null;
 
-    let newCompletedState = todo.completed ? false : true
+    let newCompletedState = todo.completed ? false : true;
     const updateQuery = `
       UPDATE todos
       SET 
         completed = ${newCompletedState},
-        completed_at = ${newCompletedState ? 'NOW()' : 'NULL'}
+        completed_at = ${newCompletedState ? "NOW()" : "NULL"}
       WHERE todos.id = $/id/
       RETURNING *
     `;
-    updatedTodo = await db.one(updateQuery, { id })
-    return updatedTodo
-
+    updatedTodo = await db.one(updateQuery, { id });
+    return updatedTodo;
   } catch (err) {
     if (recordNotFound(err) || invalidInteger(err)) {
-      return null
+      return null;
     }
-    throw (err)
+    throw err;
   }
-}
+};
 
 const getTodo = async (todoId, ownerId) => {
-  const SQL = 'SELECT * FROM todos WHERE id = $/todoId/ AND owner_id = $/ownerId/'
+  const SQL =
+    "SELECT * FROM todos WHERE id = $/todoId/ AND owner_id = $/ownerId/";
   try {
-    const todo = await db.one(SQL, { todoId, ownerId })
-    return todo
+    const todo = await db.one(SQL, { todoId, ownerId });
+    return todo;
   } catch (err) {
-    if (recordNotFound(err)) return null
-    throw (err)
+    if (recordNotFound(err)) return null;
+    throw err;
   }
-}
+};
 
 module.exports = {
   getAllTodos,
@@ -223,5 +234,5 @@ module.exports = {
   createTodo,
   removeTodo,
   updateTodo,
-  toggleCompleted
+  toggleCompleted,
 };
--- validators/todos.js
+++ validators/todos.js
@@ -1,37 +1,56 @@
-const { body, oneOf, query } = require('express-validator');
-const withValidationErrorHandler = require('./withValidationErrorHandler');
-const utils = require('../utils');
+const { body, oneOf, query } = require("express-validator");
+const withValidationErrorHandler = require("./withValidationErrorHandler");
+const utils = require("../utils");
 
 const newTodoValidators = withValidationErrorHandler([
-  body('text')
-    .trim()
-    .notEmpty(),
-  body('value')
-    .isInt({ min: 1 })
-    .trim()
-])
+  body("text").trim().notEmpty(),
+  body("value").isInt({ min: 1 }).trim(),
+]);
 
 const updateTodoValidators = withValidationErrorHandler([
   oneOf([
-    body('completed_at').isISO8601().withMessage('completed_at must be a ISO8601 date string'),
-    body('completed').isBoolean().withMessage('completed must be true or false'),
-    body('due_at').isISO8601().withMessage('due_at must be a ISO8601 date string'),
-    body('text').trim().notEmpty().withMessage('cannot be empty'),
-    body('value').isInt({ min: 1 }).withMessage('must be a valid positive number')
-  ])
-])
+    body("completed_at")
+      .isISO8601()
+      .withMessage("completed_at must be a ISO8601 date string"),
+    body("completed")
+      .isBoolean()
+      .withMessage("completed must be true or false"),
+    body("due_at")
+      .isISO8601()
+      .withMessage("due_at must be a ISO8601 date string"),
+    body("text").trim().notEmpty().withMessage("cannot be empty"),
+    body("value")
+      .isInt({ min: 1 })
+      .withMessage("must be a valid positive number"),
+    body("list_id").isInt({ min: 1 }).withMessage("list "),
+  ]),
+]);
 
 const retrieveTodosValidators = withValidationErrorHandler([
-  query('completed_at').optional().isISO8601().withMessage('must be in the format YYYY-DD-MM'),
-  query('updated_at').optional().isISO8601().withMessage('must be in the format YYYY-DD-MM'),
-  query('due_at').optional().isISO8601().withMessage('must be in the format YYYY-DD-MM'),
-  query('client_tz').optional().custom((client_tz, { req }) => utils.isValidTimeZone(client_tz)),
-  query('completed').optional().isBoolean().withMessage('must be boolean true or false'),
-  query('value').optional().isInt().withMessage('must be an integer number'),
-])
+  query("completed_at")
+    .optional()
+    .isISO8601()
+    .withMessage("must be in the format YYYY-DD-MM"),
+  query("updated_at")
+    .optional()
+    .isISO8601()
+    .withMessage("must be in the format YYYY-DD-MM"),
+  query("due_at")
+    .optional()
+    .isISO8601()
+    .withMessage("must be in the format YYYY-DD-MM"),
+  query("client_tz")
+    .optional()
+    .custom((client_tz, { req }) => utils.isValidTimeZone(client_tz)),
+  query("completed")
+    .optional()
+    .isBoolean()
+    .withMessage("must be boolean true or false"),
+  query("value").optional().isInt().withMessage("must be an integer number"),
+]);
 
 module.exports = {
   newTodoValidators,
   updateTodoValidators,
-  retrieveTodosValidators
-}
+  retrieveTodosValidators,
+};
