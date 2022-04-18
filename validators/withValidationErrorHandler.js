2c33bee015ed432e2d10b93e8f2000547ee8c4f8
alejo4373 <alejandro@flowcode.com>
2022-04-17 22:33:10 -0400

    Add first validation error message to error returned to client

--- validators/withValidationErrorHandler.js
+++ validators/withValidationErrorHandler.js
@@ -1,25 +1,24 @@
-const { validationResult } = require('express-validator');
-const { flattenErrors } = require('../utils')
+const { validationResult } = require("express-validator");
+const { flattenErrors } = require("../utils");
 
 const withValidationErrorsHandler = (validations) => {
   const handler = (req, res, next) => {
     const errors = validationResult(req);
+    const errorsArr = errors.array();
     if (!errors.isEmpty()) {
-      res
-        .status(422)
-        .json({
-          payload: {
-            errors: flattenErrors(errors.array())
-          },
-          message: 'Validation error/s',
-          error: true
-        })
+      res.status(422).json({
+        payload: {
+          errors: flattenErrors(errorsArr),
+        },
+        message: `Validation error: ${errorsArr[0]?.msg}`,
+        error: true,
+      });
     } else {
-      next()
+      next();
     }
-  }
+  };
 
-  return [validations, handler]
-}
+  return [validations, handler];
+};
 
-module.exports = withValidationErrorsHandler
+module.exports = withValidationErrorsHandler;
