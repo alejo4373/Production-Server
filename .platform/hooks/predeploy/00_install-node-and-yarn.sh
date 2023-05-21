e124e4a6f22bc7cdaf3dc3beb0899ff95f9bde43
Alejandro Franco <alejandro@flowcode.com>
2023-05-20 21:59:23 -0400

    leave debug statement

--- .platform/hooks/predeploy/00_install-node-and-yarn.sh
+++ .platform/hooks/predeploy/00_install-node-and-yarn.sh
@@ -5,6 +5,8 @@ curl https://get.volta.sh | bash
 # Install node
 # EB installs node version specified in "engines" in package.json
 # volta install node@16.14.0
+echo "af-debug" 
+volta --version
 
 # Install yarn
 volta install yarn@1.22.10
